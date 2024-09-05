a32baf090504c74b2215ff474dc47bb4c2f0a6d1
jhlee <junhui12345@gmail.com>
2024-09-05 19:44:40 +0900

    init

---
+++ src/main/java/genai/idea/fms/FmsGenaiIdeaApplication.java
@@ -0,0 +1,13 @@
+package genai.idea.fms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsGenaiIdeaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FmsGenaiIdeaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/genai/idea/fms/FmsGenaiIdeaApplicationTests.java
@@ -0,0 +1,15 @@
+package genai.idea.fms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class FmsGenaiIdeaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
